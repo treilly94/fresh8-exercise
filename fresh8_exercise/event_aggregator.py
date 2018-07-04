749a925f5c527a8395c3d76b3f211e25e98f3784
treilly94 <treilly94@live.com>
2018-07-04 17:07:08 +0100

    Drafted event aggregator

---
+++ fresh8_exercise/event_aggregator.py
@@ -0,0 +1,25 @@
+import os
+
+
+class Aggregator:
+    data = []
+    # List of files already processed
+    files = []
+
+    def file_reader(self):
+        # Get list of files in directory
+
+        # Read each file not already read in
+
+    def stats_generator(self):
+        # Total each event type
+
+        # Print to terminal
+
+    def run(self):
+        # Run every 5 seconds
+
+
+
+if __name__ == "__main__":
+    print("Event Aggregator")
