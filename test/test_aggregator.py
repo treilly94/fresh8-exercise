ce3e466fb4ed885dbb9e3707d5b8c323949f6ff5
treilly94 <treilly94@live.com>
2018-07-05 15:01:57 +0100

    Added test stats generator

--- test/test_aggregator.py
+++ test/test_aggregator.py
@@ -22,7 +22,19 @@ class TestAggregator(TestCase):
         self.assertEqual(self.a.data, expected)
 
     def test_stats_generator(self):
-        self.fail()
+        data = [
+            {"type": "Viewed", "data": {"viewId": "1", "eventDateTime": "2018-07-05T09:32:47.083473"}},
+            {"type": "Viewed", "data": {"viewId": "2", "eventDateTime": "2018-07-05T09:32:47.083507"}},
+            {"type": "Click-Through", "data": {"viewId": "2", "eventDateTime": "2018-07-05T09:32:47.083507"}},
+            {"type": "Viewed", "data": {"viewId": "3", "eventDateTime": "2018-07-05T09:32:47.083527"}},
+            {"type": "Viewed", "data": {"viewId": "4", "eventDateTime": "2018-07-05T09:32:47.083538"}},
+            {"type": "Interacted", "data": {"viewId": "4", "eventDateTime": "2018-07-05T09:32:47.083538"}}
+        ]
+        self.a.data = data
+        self.a.stats_generator()
 
-    def test_run(self):
-        self.fail()
+        expected = {'Click-Through': 1, 'Interacted': 1, 'Viewed': 4}
+        output = self.a.counts.to_dict()
+
+        self.assertEqual(output, expected)
+
